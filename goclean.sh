9427e6c6501b29d4d2fd6f90e5cc280e2a1d7192
David Hill <dhill@mindcry.org>
2019-07-16 15:06:08 -0400

    update deps and travis

---
+++ goclean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# The script does automatic checking on a Go package and its sub-packages, including:
+# 1. gofmt         (http://golang.org/cmd/gofmt/)
+# 2. go vet        (http://golang.org/cmd/vet)
+# 3. gosimple      (https://github.com/dominikh/go-simple)
+# 4. unconvert     (https://github.com/mdempsky/unconvert)
+# 5. ineffassign   (https://github.com/gordonklaus/ineffassign)
+# 6. race detector (http://blog.golang.org/race-detector)
+# 7. test coverage (http://blog.golang.org/cover)
+
+set -ex
+
+# run tests
+env GORACE="halt_on_error=1" go test -race ./...
+
+# golangci-lint (github.com/golangci/golangci-lint) is used to run each each
+# static checker.
+
+# check linters
+golangci-lint run --disable-all --deadline=10m \
+  --enable=gofmt \
+  --enable=vet \
+  --enable=gosimple \
+  --enable=unconvert \
+  --enable=ineffassign
